0340aef7cb29329792c5e2859b0fdbc09e7aba93
Jayakanth Arun <jayakantharun03@gmail.com>
2023-02-26 20:43:36 +0530

    Image Added to Database

--- routes/admin.js
+++ routes/admin.js
@@ -37,10 +37,19 @@ router.get('/add-product', function (req, res) {
   res.render("admin/add-product")
 })
 router.post('/add-product', (req, res) => {
-  console.log(req.body)
-  console.log(req.files.Image)
-  productHelpers.addProduct(req.body,(result)=>{
-    res.render("admin/add-product")
+
+  productHelpers.addProduct(req.body,(id)=>{
+    let image=req.files.Image
+    console.log(id)
+    image.mv('./public/product-images/'+id+'.jpg',(err,done)=>{
+      if (!err){
+        res.render("admin/add-product")
+      }
+      else{
+        console.log(err)
+      }
+    })
+    
   })
 })
 
