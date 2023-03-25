eecf0ed3bb412f63bd5fca85c40babe01763f5e4
Jayakanth Arun <jayakantharun03@gmail.com>
2023-03-25 23:09:37 +0530

    Updated mongodb

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ const state={
 }
 
 module.exports.connect=function(done){
-    const url='mongodb://0.0.0.0:27017'
+    const url=`mongodb+srv://jayakantharun03:click2kart@cluster0.nkgdwh3.mongodb.net/?retryWrites=true&w=majority`//'mongodb://0.0.0.0:27017'
     const dbname='shopping'
 
     mongoClient.connect( url,(err,data)=>{                   //   { useUnifiedTopology: true },
